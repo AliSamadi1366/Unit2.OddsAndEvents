24d8f731800e4caf646885719bfad24782521262
Ali Samadi <alisamadi.096@gmail.com>
2025-02-15 12:11:17 -0600

    complete the BankNumber function but still need some corrections, because does not work.

--- index.js
+++ index.js
@@ -37,6 +37,21 @@ function SortOne() {
   }
   render();
 }
+//create a function for sort all the numbers
+function SortAll() {
+  if (bank.length === 0) {
+    return;
+  }
+  while (bank.length > 0) {
+    if (number % 2 === 0) {
+      evens.push(number);
+    }
+    if (number % 2 === 1) {
+      odds.push(number);
+    }
+  }
+  render();
+}
 // === component ===
 // create a form that allows users to input a number, form has 3 buttons including "Add number" "Sort 1" "Sort All". form should be created with component function.
 //when the user clicks the "Add number" button, the number they entered into the input field, should be added to the number bank.
@@ -47,11 +62,11 @@ function NumberForm() {
         Add a number to the bank
         <input name="number" type="number" min="1" />
       </label>
-      <button>Add number</button>
-      <button>Sort 1</button>
-      <button>Sort All</button>
+      <button type="submit">Add number</button>
+      <button type="button" id="sortOne">Sort 1</button>
+      <button type="button" id="sortAll">Sort All</button>
     `;
-  $form.addEventListener("Add number", (event) => {
+  $form.addEventListener("submit", (event) => {
     // this prevents the default form behavior of refreshing the page
     event.preventDefault();
 
@@ -60,13 +75,51 @@ function NumberForm() {
     const newNumber = data.get("number");
 
     // Add that number to my state
-    addNumber(newNumber);
+    addNumber(Number(newNumber));
   });
-  // TODO: add a `Add number` event listener
+  $form.addEventListener("click", SortOne());
+  $form.addEventListener("click", SortAll());
+
   return $form;
 }
+// craete a box function for display the `odds` or `evens` numbers
+
+// The bank should display all of the numbers that the user has entered.
+
+function BankNumber() {
+  const $bank = document.createElement("section");
+  $bank.classList.add("bank");
+  const bankNumbers = bank.join(" ");
+  $bank.innerHTML =`
+  <span>${bankNumbers}</span>
+  `
+  ;
+  return $bank;
+
+}
+
 // create 3 fields with component function including "Bank" "Odds" "Evens" which stored and diplay the state changes
 
-// The number bank should display all of the numbers that the user has entered.
-
 //render the application
+function render() {
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+    <h1>Odds and Events</h1>
+    <hr>
+    <main>
+      <h2>Odds and Events</h2>
+      <NumberForm></NumberForm>
+      <p>Bank</p>
+      <div id="bank"></div>
+      <p>Odds</p>
+      <div id="odds"></div>
+      <p>Evens</p>
+      <div id="evens"></div>
+    </main>
+    `;
+  $app.querySelector("NumberForm").replaceWith(NumberForm());
+  $app.querySelector("#bank").replaceWith(BankNumber());
+  $app.querySelector("#odds").replaceWith(Box(odds));
+  $app.querySelector("#evens").replaceWith(Box(evens));
+}
+render();
